609f0a56fdb44d654311e6abbf735d3be65a8311
Alicelee1706 <78105672+Alicelee1706@users.noreply.github.com>
2022-06-04 15:27:22 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample1
+{
+    internal class Program
+    {
+        static void Main(string[] ii)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
